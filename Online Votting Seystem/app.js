189e10e50a904680945a2c94a494d5868a179506
Pranab Bhowmik <109957935+pranabbhowmik@users.noreply.github.com>
2024-12-13 13:50:07 +0530

    Update app.js

--- Online Votting Seystem/app.js
+++ Online Votting Seystem/app.js
@@ -10,7 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname("https://online-votting-system.onrender.com/");
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
